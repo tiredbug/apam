0499623835cd2c04bcfdd3079321f1568546e39e
basoro <dentix.id@gmail.com>
2021-04-20 15:40:36 +0800

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,9 @@
 //Main configuration. Silahkan sesuaikan settingan dibawah ini sesuai. Baca komentar dibelakangnya
 const nama_instansi = 'RS Masa Kini'; // Hospital Name
-const apiUrl = 'http://localhost/Khanza-Lite/api/'; // API Server URL
-const website_upload = 'http://localhost/Khanza-Lite/uploads/'; // Website Uploads Server URL
-const webapps_url = 'http://localhost/Khanza-Lite/webapps/'; // Webapps Server URL
-const token = 'lite'; // Token code for security purpose
+const apiUrl = 'https://khanza.basoro.id/api/'; // API Server URL
+const website_upload = 'https://khanza.basoro.id/uploads/'; // Website Uploads Server URL
+const webapps_url = 'https://khanza.basoro.id/webapps/'; // Webapps Server URL
+const token = 'qtbexUAxzqO3M8dCOo2vDMFvgYjdUEdMLVo341'; // Token code for security purpose
 const startDate = 0; // Start date of day for registration
 const endDate = 7; // End date of day for registration
 const debug = 1; // Ganti menjadi 0 sebelum build di phonegap.com
